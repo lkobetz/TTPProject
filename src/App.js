d6434073f216fc6f6996fc48fae8731b6957eb25
lkobetz <larissa.kobetz@gmail.com>
2020-02-25 14:25:09 -0500

    login register component added and posts new users to database

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import Login from "./Login";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {}
+      toggle: false,
+      userType: "",
+      status: false
     };
+    this.changeUserStatus = this.changeUserStatus.bind(this);
   }
   async getResponse(info) {
-    const { data } = await axios.post("/api/register", info);
+    const { data } = await axios.post("/api/login", info);
     console.log(data);
   }
+  toggleFunc(user) {
+    this.setState({ toggle: !this.state.toggle });
+    this.setState({ userType: user });
+  }
+  changeUserStatus() {
+    this.setState({ status: !this.state.status });
+  }
   render() {
+    console.log(this.state);
     return (
       <div className="App">
-        <h1>changes</h1>
-        <button
-          onClick={() =>
-            this.getResponse({
-              name: "Banana",
-              email: "banana@bing.com",
-              password: "banananaspassword"
-            })
-          }
-        >
-          log response
-        </button>
+        <button onClick={() => this.toggleFunc("new")}>Register</button>
+        <button onClick={() => this.toggleFunc("existing")}>Login</button>
+        {this.state.toggle && (
+          <Login
+            userType={this.state.userType}
+            changeUserStatus={this.changeUserStatus}
+          />
+        )}
+        {this.state.status && <h3>navigate to user's profile</h3>}
       </div>
     );
   }
---
+++ src/Login.js
@@ -0,0 +1,87 @@
+import React from "react";
+import axios from "axios";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: ""
+    };
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)} method="POST">
+        <div>
+          <input
+            className={"form_item"}
+            type="text"
+            value={this.state.name}
+            placeholder={"Name"}
+            onChange={this.onNameChange.bind(this)}
+          />
+        </div>
+        <br />
+        <div>
+          <input
+            className={"form_item"}
+            type="email"
+            aria-describedby="emailHelp"
+            value={this.state.email}
+            placeholder={"Email"}
+            onChange={this.onEmailChange.bind(this)}
+          />
+        </div>{" "}
+        <br />
+        <div>
+          <input
+            className={"form_item"}
+            type="password"
+            value={this.state.password}
+            placeholder={"Password"}
+            onChange={this.onPasswordChange.bind(this)}
+          />
+        </div>
+        <br />
+        <button type="submit" className="submit_button">
+          Submit
+        </button>
+      </form>
+    );
+  }
+  onNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  onEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  onPasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    // create another project in express and send this to that server
+    if (this.props.userType === "new") {
+      const response = await axios.post("/api/register", this.state);
+      this.resetForm();
+      if (response.status === 200) {
+        this.props.changeUserStatus();
+      }
+    } else if (this.props.userType === "existing") {
+      const response = await axios.post("/api/login", this.state);
+      this.resetForm();
+      if (response.status === 200) {
+        this.props.changeUserStatus();
+      }
+    }
+  }
+  resetForm() {
+    this.setState({ name: "", email: "", message: "" });
+  }
+}
+
+export default Login;
