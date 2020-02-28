983c7278fe70786d30f9d04310b66689eec191d4
lkobetz <larissa.kobetz@gmail.com>
2020-02-28 18:15:02 -0500

    changed path name in server index from public to build

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.use("/api", require("./api/routes.js")); // include our routes!
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 }); // Send index.html for any other requests
 
 // error handling middleware
